0fbb06d52397fc361a112bcbfd4e6bb24b598d3f
Braden <bradenstrother@gmail.com>
2022-07-11 18:38:01 -0500

    Added texture batching [-] Time.java - using glfwGetTime() due to inconsistency with windows [=] Need to add new batching for more than 8 textures

--- src/main/java/util/Time.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-public class Time {
-    public static float timeStarted = System.nanoTime(); // Time of the application start
-
-    public static float getTime() {
-        return (float) (( System.nanoTime() - timeStarted ) * 1E-9);
-    }
-}
