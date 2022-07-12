debf4cda3ed0a14f50cf53820be1024aba018f38
Braden <bradenstrother@gmail.com>
2022-07-12 17:36:31 -0500

    Setup ImGui TODO: Integrate font rendering

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -10,10 +10,11 @@ import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class Window {
-    private final int width;
-    private final int height;
+    private int width;
+    private int height;
     private final String title;
     private long glfwWindow; // Memory address where the glfwCreateWindow() is created.
+    private ImGuiLayer imGuiLayer;
 
     public float r, g, b, a;
 
@@ -101,6 +102,10 @@ public class Window {
         glfwSetMouseButtonCallback(glfwWindow, MouseListener::mouseButtonCallback);
         glfwSetScrollCallback(glfwWindow, MouseListener::mouseScrollCallback);
         glfwSetKeyCallback(glfwWindow, KeyListener::keyCallback);
+        glfwSetWindowSizeCallback(glfwWindow, (w, newWidth, newHeight) -> {
+           Window.setWidth(newWidth);
+           Window.setHeight(newHeight);
+        });
 
         // Make the OpenGl context current.
         glfwMakeContextCurrent(glfwWindow);
@@ -119,6 +124,8 @@ public class Window {
 
         glEnable(GL_BLEND);
         glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+        this.imGuiLayer = new ImGuiLayer(glfwWindow);
+        this.imGuiLayer.initImGui();
 
         Window.changeScene(0);
     }
@@ -139,11 +146,28 @@ public class Window {
                 currentScene.update(dt);
             }
 
+            this.imGuiLayer.update(dt);
             glfwSwapBuffers(glfwWindow);
 
             endTime = (float) glfwGetTime();
             dt = endTime - beginTime;
             beginTime = endTime;
         }
+    }
+
+    public static int getWidth() {
+        return get().width;
+    }
+
+    public static int getHeight() {
+        return get().height;
+    }
+
+    public static void setWidth(int newWidth) {
+        get().width = newWidth;
+    }
+
+    public static void setHeight(int newHeight) {
+        get().height = newHeight;
     }
 }
